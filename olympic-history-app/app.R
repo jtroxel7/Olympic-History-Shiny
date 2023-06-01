454ca735f5f2818a8bc27b19c52f0975ee2ab48a
Jack Troxel <jtroxel7@users.noreply.github.com>
2023-06-01 14:27:52 -0500

    work

--- olympic-history-app/app.R
+++ olympic-history-app/app.R
@@ -113,9 +113,45 @@ ui <- fluidPage(
       ),
       tabPanel(
         title = "Body Size vs. Sport",
-        titlePanel("Body Size vs. Olympic Sport"),
-        mainPanel(
-          p("PUT GRAPHIC HERE")
+        titlePanel("Distribution of Body Size by Sport"),
+        sidebarLayout(position = "right",
+            sidebarPanel(
+              selectInput("sport", 
+                          label = "Select Sport",
+                          choices = c("All Sports","Alpine Skiing",               
+                                      "Archery","Art Competitions","Athletics",                
+                                      "Badminton","Baseball","Basketball",               
+                                      "Beach Volleyball","Biathlon",                 
+                                      "Bobsleigh","Boxing","Canoeing" ,                
+                                      "Cross Country Skiing" ,    
+                                      "Curling","Cycling","Diving",                   
+                                      "Equestrianism","Fencing","Figure Skating",          
+                                      "Football","Freestyle Skiing","Golf",                     
+                                      "Gymnastics","Handball","Hockey",                   
+                                      "Ice Hockey","Jeu De Paume","Judo",                     
+                                      "Lacrosse","Luge",     
+                                      "Modern Pentathlon","Nordic Combined",          
+                                      "Polo","Rhythmic Gymnastics",      
+                                      "Rowing","Rugby",                    
+                                      "Rugby Sevens","Sailing","Shooting",                 
+                                      "Short Track Speed Skating","Skeleton","Ski Jumping",              
+                                      "Snowboarding","Softball","Speed Skating",            
+                                      "Swimming","Synchronized Swimming","Table Tennis",           
+                                      "Taekwondo","Tennis","Trampolining",             
+                                      "Triathlon","Tug-Of-War","Volleyball",               
+                                      "Water Polo","Weightlifting","Wrestling"),
+                          selected = "All Sports"),
+              radioButtons("button",
+                           "Select Attribute:",
+                           choices = c("Height", 
+                                       "Weight"),
+                           selected = "Height"),
+              p(HTML("<small><em>Note: Not all Olympic sports are played by both sexes.</em></small>"))
+              ),
+            
+            mainPanel(
+              plotOutput(outputId = "height_weight_plot")
+            )
         )
       ),
       tabPanel(
@@ -130,7 +166,17 @@ ui <- fluidPage(
             help users understand."),
           br(),
           h4("Partner Work Split"),
-          p()
+          p("We decided to work on this project so that we could
+            create a more informative dashboard than either of us would've been
+            able to achieve on our own. In general, we both built of of eachother's 
+            strengths and worked together on most aspects of the project. This
+            allowed us to explore in depth aspects such as CSS styling for the 
+            dashboard. In terms of a specific work split, Charlie was
+            focused on the two map graphics (both static and with widgets) while
+            Jack focused on visualizing the distributions of body sizes by sport.
+            We both believe that the project that we were able to complete is
+            significantly more in depth and informational than we would have
+            been able to achieve as individuals.")
         )
       )
     )
@@ -139,6 +185,84 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   # Server logic goes here
+  
+  # create dist plot for heights and weights by sport
+  output$height_weight_plot <- renderPlot({
+    
+    if (input$sport == "All Sports") {
+      sport_data <- data
+    } else if (input$sport != "") {
+      sport_data <- subset(data, sport %in% input$sport)
+    } else {
+      sport_data <- data
+    }
+    
+    x <- case_when(
+      input$button == "Height" ~pull(sport_data, height),
+      input$button == "Weight" ~pull(sport_data, weight)
+    )
+    
+    x_lab <- case_when(
+      input$button == "Height" ~"Height in Centimeters",
+      input$button == "Weight" ~"Weight in Kilograms"
+    )
+    
+    x_lim <- case_when(
+      input$button == "Height" ~c(125,225),
+      input$button == "Weight" ~c(35,135)
+    )
+    
+    subtitle <- case_when(
+      input$button == "Height" ~"Height Distribution",
+      input$button == "Weight" ~"Weight Distribution"
+    )
+    
+    if (input$sport == "All Sports") {
+      title <- "All Olympic Sports"
+    } else if (input$sport != "") {
+      title <- paste("Olympic", input$sport)
+    } else {
+      title <- "All Olympic Sports"
+    }
+    
+    
+    ggplot(data = sport_data, 
+           aes(x = x, 
+               color = sex, 
+               fill = sex)) +
+      geom_density(alpha = 0.25) +
+      theme_minimal() +
+      scale_x_continuous(
+        name = x_lab,
+        limits = x_lim
+      ) +
+      scale_y_continuous(
+        name = NULL,
+        labels = NULL,
+        expand = c(0, 0)
+      ) +
+      scale_color_manual(
+        values = c("#89cffa", "#f7a1c8"),
+        labels = c("Male", "Female")
+      ) +
+      scale_fill_manual(
+        values = c("#89cffa", "#f7a1c8"),
+        labels = c("Male", "Female")
+      ) +
+      labs(
+        title = title,
+        subtitle = subtitle
+      ) +
+      theme(
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(face = "bold", size = 14),
+        plot.subtitle = element_text(size = 12),
+        legend.title = element_blank(),
+        legend.position = c(0.85, 0.9), 
+        legend.justification = c(1, 1)
+      )
+  })
 }
 
 shinyApp(ui, server)
